8008ceeeb084382885b8933bae325c42c649e112
SurajMondem <surajmondem@gmail.com>
2022-02-12 16:58:37 -0500

    Added Complete code

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-console.log('Hello world')
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+const PORT = 3000
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', {roomId: req.params.room})
+})
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).emit('user-connected', userId)
+  })
+
+  socket.on('disconnect', (roomId, userId) => {
+    console.log('disconnected', roomId, userId);
+    socket.to(roomId).emit('user-disconnected', userId)
+  })
+})
+
+server.listen(PORT) 
